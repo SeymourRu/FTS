d7665d5ba914004a2722e5eadd8c471cee3617d7
Seymour <ilyamalinaya@mail.ru>
2018-08-09 05:34:21 +0300

    Add module SDK

---
+++ FTSModuleSDK/IModule.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FTSModuleSDK
+{
+    public interface IModule
+    {
+        void Configure();
+        void Run();
+    }
+}
